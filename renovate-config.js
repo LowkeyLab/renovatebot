9f1e093a14e708262f1767adc8262fd8563c7423
tacascer <trandangtrithanh2000@gmail.com>
2026-04-13 23:30:07 +0100

    fix: migrate config

--- renovate-config.js
+++ renovate-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  branchPrefix: "lowkeylab-renovate/",
-  repositories: ["LowkeyLab/bazel-repo", "LowkeyLab/renovatebot"],
-  platformCommit: true,
-  allowedUnsafeExecutions: ["bazelModDeps"],
+   branchPrefix: "lowkeylab-renovate/",
+   repositories: ["LowkeyLab/bazel-repo", "LowkeyLab/renovatebot"],
+   platformCommit: "enabled",
+   allowedUnsafeExecutions: ["bazelModDeps"]
 };
 
