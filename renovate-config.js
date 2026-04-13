91845fea30f0822073aa0120ff23fb5e5d303e54
tacascer <trandangtrithanh2000@gmail.com>
2026-04-13 23:27:30 +0100

    fix: update branch prefix

--- renovate-config.js
+++ renovate-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  branchPrefix: "lowkeylab-renovate/",
   repositories: ["LowkeyLab/bazel-repo", "LowkeyLab/renovatebot"],
   platformCommit: true,
   allowedUnsafeExecutions: ["bazelModDeps"],
