b3ea41d4661a7925f1b2c4c7bb9532543c8c0f01
tacascer <trandangtrithanh2000@gmail.com>
2026-04-13 23:34:57 +0100

    fix: allow updating maven in bazel

--- renovate-config.js
+++ renovate-config.js
@@ -2,6 +2,7 @@ module.exports = {
    branchPrefix: "lowkeylab-renovate/",
    repositories: ["LowkeyLab/bazel-repo", "LowkeyLab/renovatebot"],
    platformCommit: "enabled",
-   allowedUnsafeExecutions: ["bazelModDeps"]
+   allowedUnsafeExecutions: ["bazelModDeps"],
+  allowedCommands: ["REPIN=1 bazel run @maven//:pin"],
 };
 
