e66ce10ea49bd17b7fd400e6fa80859046904066
tacascer <trandangtrithanh2000@gmail.com>
2026-04-13 23:39:59 +0100

    fix: add gradle build scan

--- renovate-config.js
+++ renovate-config.js
@@ -1,6 +1,6 @@
 module.exports = {
    branchPrefix: "lowkeylab-renovate/",
-   repositories: ["LowkeyLab/bazel-repo", "LowkeyLab/renovatebot"],
+   repositories: ["LowkeyLab/bazel-repo", "LowkeyLab/renovatebot", "LowkeyLab/gradle-build-scan-server"],
    platformCommit: "enabled",
    allowedUnsafeExecutions: ["bazelModDeps"],
   allowedCommands: ["REPIN=1 bazel run @maven//:pin"],
