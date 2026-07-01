0d10d4d48038473a4cde7a21a34f14e098b3933e
Tim Tran <49887921+tacascer@users.noreply.github.com>
2026-07-01 22:40:18 +0100

    fix: allow unsetting android home for maven pin (#19)

--- renovate-config.js
+++ renovate-config.js
@@ -4,6 +4,11 @@ module.exports = {
   branchPrefix: "lowkeylab-renovate/",
   platformCommit: "enabled",
   allowedUnsafeExecutions: ["bazelModDeps"],
-  allowedCommands: ["REPIN=1 bazel run @maven//:pin", "bazel mod deps *", "bazel run *"],
+  allowedCommands: [
+    "REPIN=1 bazel run @maven//:pin",
+    "env -u ANDROID_HOME REPIN=1 bazel run @maven//:pin *",
+    "bazel mod deps *",
+    "bazel run *",
+  ],
 };
 
