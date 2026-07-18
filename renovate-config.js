03a07052c4bf1f03a78c048e6543d28b5a408625
tacascer <trandangtrithanh2000@gmail.com>
2026-07-18 01:41:58 +0100

    fix: expose Android SDK to Renovate commands

--- renovate-config.js
+++ renovate-config.js
@@ -4,6 +4,7 @@ module.exports = {
   branchPrefix: "lowkeylab-renovate/",
   platformCommit: "enabled",
   allowedUnsafeExecutions: ["bazelModDeps"],
+  customEnvVariables: { ANDROID_HOME: "/opt/android-sdk" },
   allowedCommands: ["REPIN=1 bazel run @maven//:pin", "bazel mod deps *", "bazel run *"],
 };
 
